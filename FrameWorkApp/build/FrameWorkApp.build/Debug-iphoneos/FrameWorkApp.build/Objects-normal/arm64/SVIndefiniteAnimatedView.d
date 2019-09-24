cb7f9a5b551724b5785321b7cf7c196e711acc6a
IMiMineDigital <48542324+IMiMineDigital@users.noreply.github.com>
2019-09-24 10:37:34 +0530

    Commit3

--- FrameWorkApp/build/FrameWorkApp.build/Debug-iphoneos/FrameWorkApp.build/Objects-normal/arm64/SVIndefiniteAnimatedView.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/kamleshprajapati/Desktop/FrameWorkApp/FrameWorkApp/FrameWorkApp/SVProgressHUD/SVProgressHUD/SVIndefiniteAnimatedView.m \
-  /Users/kamleshprajapati/Desktop/FrameWorkApp/FrameWorkApp/build/FrameWorkApp.build/Debug-iphoneos/FrameWorkApp.build/module.modulemap \
-  /Users/kamleshprajapati/Desktop/FrameWorkApp/FrameWorkApp/FrameWorkApp/SVProgressHUD/SVProgressHUD/SVIndefiniteAnimatedView.h \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS13.0.sdk/usr/include/libxml2/module.modulemap \
-  /Users/kamleshprajapati/Desktop/FrameWorkApp/FrameWorkApp/FrameWorkApp/SVProgressHUD/SVProgressHUD/SVProgressHUD.h
